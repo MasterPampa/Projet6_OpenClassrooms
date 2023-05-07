dc5a0b820811e41ec53f4ec0751117ab8eb0a94f
Master Pampa <styven.munhurrun@gmail.com>
2023-05-07 21:34:09 +0200

    ajout filtre et script filtre

--- FrontEnd/assets/scripts/gallery.js
+++ FrontEnd/assets/scripts/gallery.js
@@ -1,26 +1,69 @@
-async function getGallery() {
+let resultats=[];
 
-const reponse = await fetch("http://localhost:5678/api/works");
-const resultats = await reponse.json();
-console.log(resultats);
+async function getGallery(resultats) {
 
-for (let i = 0; i < resultats.length; i++){
+    const reponse = await fetch("http://localhost:5678/api/works");
+    resultats = await reponse.json();
+    console.log(resultats);
 
-const travaux = resultats[i];
+    for (let i = 0; i < resultats.length; i++){
 
-const sectionGallery = document.querySelector(".gallery");
+        const travaux = resultats[i];
 
-const figureElement = document.createElement("figure");
-const imageElement = document.createElement("img");
-imageElement.src = travaux.imageUrl;
-const figcaptionElement = document.createElement('figcaption');
-figcaptionElement.textContent = travaux.title;
+        const sectionGallery = document.querySelector(".gallery");
 
-sectionGallery.appendChild(figureElement)
-figureElement.appendChild(imageElement);
-figureElement.appendChild(figcaptionElement);
+        const figureElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.src = travaux.imageUrl;
+        const figcaptionElement = document.createElement('figcaption');
+        figcaptionElement.textContent = travaux.title;
 
-
+        sectionGallery.appendChild(figureElement)
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figcaptionElement);
+    }
 }
+getGallery(resultats);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function getCategories() {
+    const reponse = await fetch("http://localhost:5678/api/categories");
+    categoriesListe = await reponse.json();
+
+    for (let i = 0; i < categoriesListe.length; i++){
+
+        categories = categoriesListe[i];
+
+        const sectionCategories = document.querySelector(".filter");
+
+        const filterElement = document.createElement("div");
+
+        filterElement.id = "" + categories.id;
+        filterElement.classList.add("filter__buttons");
+        filterElement.textContent = categories.name;
+
+        sectionCategories.appendChild(filterElement);
+    }
 }
-getGallery();
+getCategories();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+window.addEventListener('load', function() {
+
+    const filterButtons = document.querySelectorAll('.filter__buttons');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const clickedId = button.id;
+            console.log('Clicked ID:', clickedId);
+
+            let imagesFiltrees = resultats.filter(function (image){
+                return image.categoryId == clickedId;
+            });
+            document.querySelector(".gallery").innerHTML ="";
+            getGallery(imagesFiltrees);
+        });
+    });
+});
