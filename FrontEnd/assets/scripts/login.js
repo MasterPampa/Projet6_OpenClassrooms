fa7886c325a6551db9c1a8ed3f2a9e3ad9b06463
Master Pampa <styven.munhurrun@gmail.com>
2023-05-08 21:23:34 +0200

    correction complete login

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,55 +1,24 @@
 window.addEventListener('load', function() {
 
-    login.addEventListener('submit', function(e) {
+    const login = document.getElementById("login");
 
-        let login = document.getElementById("login");
-        let username = document.getElementById("username");
-        let password = document.getElementById("password");
-        let idTest = "sophie.bluel@test.tld";
-        let passwordTest = "S0phie";
-        let usernameError = document.getElementById("usernameError");
-        let passwordError = document.getElementById("passwordError");
-        let usernameOk = 0;
-        let passwordOk = 0;
-
-        ////////////////USERNAME/////////////////////
-        
-        if (username.value.trim() == ""){
-            usernameError.innerHTML = "Merci de renseigner votre adresse email.";
-            usernameError.style.color = 'red';
-            e.preventDefault();
-        } else if (username.value != idTest) {
-            console.log(username.value);
-            usernameError.innerHTML = "Merci de renseigner une adresse email valide.";
-            usernameError.style.color = 'red';
-            e.preventDefault();
-        } else if (username.value === idTest) {
-            usernameError.innerHTML = "";
-            usernameOk == 1;
-        }
-
-        ///////////////PASSWORD///////////////////////
-
-        if (password.value == ""){
-            passwordError.innerHTML = "Merci de renseigner votre mot de passe.";
+    login.addEventListener('submit', async (e) => {
+        const email = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        e.preventDefault();
+        const response = await fetch("http://" + window.location.hostname + ":5678/api/users/login", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ email, password }),
+    });
+        if (response.ok) {
+            const data = await response.json();
+            sessionStorage.setItem('accessToken', data.token);
+            window.location.href="http://" + window.location.hostname + ":5500/FrontEnd/index.html";
+        } else {
+            const passwordError = document.getElementById('passwordError');
+            passwordError.innerText = "Erreur dans l’identifiant ou le mot de passe";
             passwordError.style.color = 'red';
-            e.preventDefault();
-        } else if (password.value != passwordTest) {
-            console.log(password.value);
-            passwordError.innerHTML = "Merci de renseigner un mot de passe valide.";
-            passwordError.style.color = 'red';
-            e.preventDefault();
-        } else if (password.value === passwordTest) {
-            passwordError.innerHTML = "";
-            passwordOk = 1;
-
-        }
-        
-        //////////////SI TOUT EST OK ENVOI DE LA REQUETE///////////////
-
-        if (passwordOk === usernameOk){
-            SubmitEvent;
         }
     });
-
 });
